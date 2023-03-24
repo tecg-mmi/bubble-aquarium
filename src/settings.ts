a8f2cbaff5eec48cce72be1b6fce72dce1ce7f93
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-24 11:52:21 +0100

    :tada: Initial commit

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,8 @@
 export const settings = {
     bubble: {
         maxCount: 10,
-        radius:{min:2,max:5},
-        speed:{min:1,max:1},
+        radius: {min: 2, max: 8},
+        speed: {min: 1, max: 7},
     }
 
 };
