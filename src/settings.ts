f460a860758993103e9730131b40d3a9b3b90416
Daniel Schreurs <daniel.schreurs@hotmail.com>
2023-03-24 11:45:20 +0100

    :tada: Initial commit

---
+++ src/settings.ts
@@ -0,0 +1,8 @@
+export const settings = {
+    bubble: {
+        maxCount: 10,
+        radius:{min:2,max:5},
+        speed:{min:1,max:1},
+    }
+
+};
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,18 +3,19 @@ import {Hsl} from "../../../canvas-framework-23/src/ts/Colors/Hsl";
 import {Animate} from "../../../canvas-framework-23/src/ts/Animate";
 import {Random} from "../../../canvas-framework-23/src/ts/Helpers/Random";
 import {Bubble} from "./Bubble";
+import {settings} from "../settings";
 
 function main() {
     const canvas = new Canvas(document.getElementById('my-canvas') as HTMLCanvasElement, 300, 900);
     const animation = new Animate(canvas);
-    for (let i = 0; i < 20; i++) {
-        const radius = Random.nextInt(3, 8);
+    for (let i = 0; i < settings.bubble.maxCount; i++) {
+        const radius = Random.nextIntMinMax(settings.bubble.radius);
         const bubble = new Bubble({
             canvas: canvas,
             color: new Hsl(50, 70, 50),
             radius: radius,
             position: {x: Random.nextInt(radius, canvas.width - radius), y: canvas.height + radius * 2},
-            speed: Random.nextInt(3, 7),
+            speed: Random.nextIntMinMax(settings.bubble.speed),
             direction: 0
         });
         animation.registerForAnimation(bubble);
